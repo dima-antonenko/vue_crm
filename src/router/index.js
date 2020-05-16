bdbec336d8f33abf20faa8dafe94b94f2abf2dc1
dima <antonenkodima12@gmail.com>
2020-05-16 14:44:16 +0300

    refactoring router

--- src/router/index.js
+++ src/router/index.js
@@ -1,70 +1,60 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import Categories from '../views/Categories.vue'
-import Detail from '../views/Detail.vue'
-import History from '../views/History.vue'
-import Planning from '../views/Planning.vue'
-import Profile from '../views/Profile.vue'
-import Record from '../views/Record.vue'
-import Register from '../views/Register.vue'
-
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: import('../views/Home')
   },
     {
       path: '/login',
       name: 'login',
       meta: {layout: 'empty'},
-      component: Login
+      component: () => import('../views/Login')
     },
     {
       path: '/categories',
       name: 'categories',
       meta: {layout: 'main'},
-      component: Categories
+      component: () => import('../views/Categories')
     },
     {
       path: '/detail',
       name: 'detail',
       meta: {layout: 'main'},
-      component: Detail
+      component: () => import('../views/Detail')
     },
     {
       path: '/history',
       name: 'history',
       meta: {layout: 'main'},
-      component: History
+      component: () => import('../views/History')
     },
     {
       path: '/planning',
       name: 'planning',
       meta: {layout: 'main'},
-      component: Planning
+      component: () => import('../views/Planning')
     },
     {
       path: '/profile',
       name: 'profile',
       meta: {layout: 'main'},
-      component: Profile
+      component: () => import('../views/Profile')
     },
     {
       path: '/record',
       name: 'record',
       meta: {layout: 'main'},
-      component: Record
+      component: () => import('../views/Record')
     },
     {
       path: '/register',
       name: 'register',
       meta: {layout: 'empty'},
-      component: Register
+      component: () => import('../views/Register')
     }
 ]
 
